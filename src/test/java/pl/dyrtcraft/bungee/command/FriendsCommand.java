4f8db9dbe516088f929e3b851e64e253cada9c1d
TheMolkaPL <makietah0@gmail.com>
2013-10-12 18:31:12 +0200

    Release 1.2 - SNAPSHOT BUILD 03
    Dodano pomoc dla operatorów w komendzie /friends

--- src/test/java/pl/dyrtcraft/bungee/command/FriendsCommand.java
+++ src/test/java/pl/dyrtcraft/bungee/command/FriendsCommand.java
@@ -23,6 +23,7 @@ public class FriendsCommand extends Command {
 			}
 			ProxiedPlayer player = (ProxiedPlayer) sender;
 			FriendsCommand.showOnline(player);
+			return;
 		}
 		if(args.length==1) {
 			DyrtCraftBungee.debug(sender.getName() + " issued DyrtCraftBungee command /friends or /fr " + args); // Debug
@@ -33,11 +34,18 @@ public class FriendsCommand extends Command {
 			ProxiedPlayer player = (ProxiedPlayer) sender;
 			if(args[0].equalsIgnoreCase("-list") || args[0].equalsIgnoreCase("lista") || args[0].equalsIgnoreCase("pokaz") || args[0].equalsIgnoreCase("show")) {
 				FriendsCommand.show(player);
+				return;
 			}
 			if(args[0].equalsIgnoreCase("-add") || args[0].equalsIgnoreCase("-delete") || args[0].equalsIgnoreCase("-remove")) {
 				erArg(sender);
-			} else {
+				return;
+			}
+			if(sender.hasPermission("dyrtcraft.operator")) {
 				FriendsCommand.show(ProxyServer.getInstance().getPlayer(args[0]));
+				return;
+			} else {
+				erArg(sender);
+				return;
 			}
 		}
 		if(args.length==2) {
@@ -49,14 +57,18 @@ public class FriendsCommand extends Command {
 			ProxiedPlayer player = (ProxiedPlayer) sender;
 			if(args[0].equalsIgnoreCase("-add") || args[0].equalsIgnoreCase("-dodaj") || args[0].equalsIgnoreCase("-zapros")) {
 				FriendsCommand.add(player, args[1]);
+				return;
 			}
 			if(args[0].equalsIgnoreCase("-delete") || args[0].equalsIgnoreCase("-remove") || args[0].equalsIgnoreCase("-usun") || args[0].equalsIgnoreCase("-wyrzuc")) {
 				FriendsCommand.remove(player, args[0]);
+				return;
 			} else {
 				erArg(sender);
+				return;
 			}
 		} else {
 			erArg(sender);
+			return;
 		}
 	}
 	
@@ -90,8 +102,18 @@ public class FriendsCommand extends Command {
 	
 	
 	protected void erArg(CommandSender sender) {
-		sender.sendMessage(ChatColor.RED + "Podales/as nieprawidlowa liczbe argumentów!");
-		sender.sendMessage(ChatColor.RED + "Uzycie: /friends [-dodaj|-lista|-usun] [gracz]");
+		if(sender.hasPermission("dyrtcraft.operator")) {
+			sender.sendMessage(ChatColor.RED + "/fr " + ChatColor.GOLD + "Znajomi online");
+			sender.sendMessage(ChatColor.RED + "/fr <gracz> " + ChatColor.GOLD + "Wszyscy znajomi gracza \"gracz\"");
+			sender.sendMessage(ChatColor.RED + "/fr -add <gracz> " + ChatColor.GOLD + "Dodaj znajomego");
+			sender.sendMessage(ChatColor.RED + "/fr -list " + ChatColor.GOLD + "Lista znajomych");
+			sender.sendMessage(ChatColor.RED + "/fr -remove <gracz> " + ChatColor.GOLD + "Usun znajomego");
+			return;
+		} else {
+			sender.sendMessage(ChatColor.RED + "Podales/as nieprawidlowa liczbe argumentów!");
+			sender.sendMessage(ChatColor.RED + "Uzycie: /friends [-dodaj|-lista|-usun] [gracz]");
+			return;
+		}
 	}
 	
 }
